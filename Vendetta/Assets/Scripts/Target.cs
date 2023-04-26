8a23860ab617a8be623b6d06b60feadf812e049f
DuarteMendonca24 <durtemendonca1234@gmail.com>
2023-04-27 00:37:16 +0100

    Enemy Shooting and Player Damage
    Enemy shoots very fast and player dies , needs fixing

--- Vendetta/Assets/Scripts/Target.cs
+++ Vendetta/Assets/Scripts/Target.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem.XR;
+
 
 public class Target : MonoBehaviour
 {
-    public float health = 50f;
+    public float health = 50.0f;
+    public TextMeshProUGUI healthDisplay;
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        healthDisplay.text = "Health : " + health.ToString();
+        
+    }
 
     //public function because we will need to call it from the gun script
     public void TakeDamage (float amount)
